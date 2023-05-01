ce3411353d90853cc63554d8e3083918d45c04b9
Vova <bordiian.v@gmail.com>
2023-05-01 13:51:52 +0300

    slightly fixed styles in trainings route

--- app/trainings/page.tsx
+++ app/trainings/page.tsx
@@ -30,9 +30,9 @@ function Trainings() {
   return (
     <div className='flex flex-wrap'>
       <button onClick={getTrainings}>Get Trainings</button>
-      <div id='trainings' className='flex justify-content-between gap-6 bg-gray-400 h-12'>
+      <div id='trainings' className='flex flex-wrap justify-center items-center p-5 gap-5'>
         {trainings.map((training: Training) => (
-          <div key={training._id}>
+          <div key={training._id} className='w-full md:w-1/2 lg:w-1/4 border border-gray-300 shadow-md rounded-lg p-4'>
             <h4>{training.title}</h4>
             <div>{training.exercises.map((exercise) => (
               <p key={exercise.position}>{exercise.position} {exercise.name}</p>
