fdbb8da91adc1d09ca276d6f580670146694fc78
Vova <bordiian.v@gmail.com>
2023-05-05 13:44:44 +0300

    added redirect to login page before watching trainings

--- app/trainings/page.tsx
+++ app/trainings/page.tsx
@@ -33,21 +33,30 @@ function Trainings() {
 
   
   
-
-  return (
-    <div className='flex flex-wrap'>
-      <div id='trainings' className='flex flex-wrap justify-center items-center p-5 gap-5'>
-        {trainings.map((training: Training) => (
-          <div key={training._id} className='w-full md:w-1/2 lg:w-1/4 border border-gray-300 shadow-md rounded-lg p-4'>
-            <h4>{training.title}</h4>
-            <div>{training.exercises.map((exercise) => (
-              <p key={exercise.position}>{exercise.position} {exercise.name}</p>
-            ))}</div>
-          </div>
-        ))}
+  if (trainings.length >= 1) {
+    return (
+      <div className='flex flex-wrap'>
+        <div id='trainings' className='flex flex-wrap justify-center items-center p-5 gap-5'>
+          {trainings.map((training: Training) => (
+            <div key={training._id} className='w-full md:w-1/2 lg:w-1/4 border border-gray-300 shadow-md rounded-lg p-4'>
+              <h4>{training.title}</h4>
+              <div>{training.exercises.map((exercise) => (
+                <p key={exercise.position}>{exercise.position} {exercise.name}</p>
+              ))}</div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+      return (
+        <div className='flex flex-wrap'>
+          <div id='trainings' className='flex flex-wrap justify-center items-center p-5 gap-5'>
+            <h1>You Have not trainings yet!!!</h1>
+          </div>
+        </div>
+      )
+  }
 }
 
 export default Trainings
