22e4ad1ceb755174b63f3313ec559dc8fc24a90c
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-06-26 15:32:03 +0300

    fixed one more incorrect url

--- app/auth/sign/page.tsx
+++ app/auth/sign/page.tsx
@@ -3,7 +3,7 @@ import { useState, ChangeEvent, MouseEvent } from 'react'
 import { signIn } from 'next-auth/react'
 
 
-function Sign() {
+function Sign () {
   const [formData, setFormData] = useState({
     username: '',
     password: '',
@@ -17,13 +17,13 @@ function Sign() {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    })) 
+    }))
   }
 
   const onSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     if (password !== confirmPassword) return console.log('Passwords does not match!')
-    const createdUser = await fetch(`http://localhost:3500/auth/register`, {
+    const createdUser = await fetch(`${process.env.API_URL}/auth/register`, {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
@@ -31,7 +31,7 @@ function Sign() {
       body: JSON.stringify({
         username,
         password
-      })  
+      })
     })
     console.log(createdUser)
 
@@ -42,50 +42,50 @@ function Sign() {
         redirect: true,
         callbackUrl: '/'
       })
-  } else {
-    console.log('Error, didnt create!')
+    } else {
+      console.log('Error, didnt create!')
+    }
   }
-}
 
 
 
   return (
     <form className="bg-white p-6 rounded-lg shadow-md">
-  <div className="mb-4">
-    <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
-      Username
-    </label>
-    <input
-      className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="username" type="text" placeholder="Enter your username" 
-      onChange={onChange}/>
-  </div>
-  <div className="mb-4">
-    <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
-      Password
-    </label>
-    <input
-      className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="password" type="password" placeholder="Enter your password" 
-      onChange={onChange}/>
-  </div>
-  <div className="mb-6">
-    <label className="block text-gray-700 font-bold mb-2" htmlFor="confirmPassword">
-      Confirm Password
-    </label>
-    <input
-      className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-      id="confirmPassword" type="password" placeholder="Confirm your password"
-      onChange={onChange}/>
-  </div>
-  <div className="flex items-center justify-between">
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      type="submit"  onClick={onSubmit}>
-      Sign Up
-    </button>
-  </div>
-</form>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
+          Username
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Enter your username"
+          onChange={onChange} />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="password" type="password" placeholder="Enter your password"
+          onChange={onChange} />
+      </div>
+      <div className="mb-6">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="confirmPassword">
+          Confirm Password
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="confirmPassword" type="password" placeholder="Confirm your password"
+          onChange={onChange} />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit" onClick={onSubmit}>
+          Sign Up
+        </button>
+      </div>
+    </form>
   )
 }
 
